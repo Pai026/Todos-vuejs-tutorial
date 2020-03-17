38f10fc705139f19baa2dbbeb465febdc18f0d51
Abhiram Pai <abhirampai1999@gmail.com>
2020-03-17 18:30:19 +0530

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,42 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos'
-
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import axios from 'axios';
 export default {
   name: 'App',
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
+    
   },
   data(){
     return{
       todos:[
-        {
-          id:1,
-          title:"Todo One",
-          completed:false
-
-      },
-      {
-        id:2,
-        title:"Todo Two",
-        completed:true
-      },
-      {
-        id:3,
-        title:"Todo Three",
-        completed:false
-      }
       ]
     }
   },
   methods:{
     deleteTodo(id){
       this.todos=this.todos.filter(todo=>todo.id!==id);
+    },
+    addTodo(newTodo){
+      this.todos=[...this.todos,newTodo];
     }
+  },
+  created(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=7')
+    .then(res => this.todos = res.data)
+    .catch(err=>console.log(err))
   }
 
 }
@@ -52,5 +51,17 @@ export default {
 body{
   font-family:Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+.btn
+{
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: white;
+  padding:7px 20px;
+  cursor: pointer;
+}
+.btn:hover{
+  background:#666;
 }
 </style>
